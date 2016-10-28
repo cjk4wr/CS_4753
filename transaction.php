97f9bd04c0c037bd6f5e23c41381e81b853ecd9c
Christine <cjk4wr@virginia.edu>
2016-10-28 16:47:20 -0400

    merge fix

--- transaction.php
+++ transaction.php
@@ -162,11 +162,6 @@ $cardissue = false;
 							<?php if($cardissue == false) {?>
 							<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 							<input type="hidden" name="cmd" value="_s-xclick">
-<<<<<<< HEAD
-							<input type="hidden" name="hosted_button_id" value="UWYGF6T6MXZEY">
-=======
-							<div class="container">
->>>>>>> b7e7f84323afe8595b8013bc0441b82436c797b6
 							<table>
 							<tr><td><input type="hidden" name="on0" value="Subscriptions">Subscriptions</td></tr><tr><td><select name="os0">
 								<option value="Weekly Subscription">Weekly Subscription $9.99 USD</option>
@@ -179,8 +174,6 @@ $cardissue = false;
 							<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 							<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 							</form>
-
-
 							<?php } ?>
 							</div>
 				</div>
