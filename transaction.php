cb229143b1bdab954e265552a294dc2f3ec77f1e
Christine <cjk4wr@virginia.edu>
2016-10-28 17:01:37 -0400

    fixed button

--- transaction.php
+++ transaction.php
@@ -154,29 +154,45 @@ $cardissue = false;
 						<!-- Content -->
 							<article id="content">
 								<header>
-									 <h3 align="center"><strong> <?php echo $string; ?> <strong></h3> <br>
+									 <h3 align="center"><strong> <?php echo $string; ?> <strong></h3>
 								</header>
 								<!-- Originally 3 paragraphs using <p>; commented out b/c unnecessary for us as of now -->
 							</article>
 					</div>
 							<?php if($cardissue == false) {?>
-							<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-							<input type="hidden" name="cmd" value="_s-xclick">
-							<div class="container">
-							<table>
-							<tr><td><input type="hidden" name="on0" value="Subscriptions">Subscriptions</td></tr><tr><td><select name="os0">
-								<option value="Weekly Subscription">Weekly Subscription $9.99 USD</option>
-								<option value="Monthly Subscription">Monthly Subscription $29.99 USD</option>
-								<option value="Semester long Subscription">Semester long Subscription $99.99 USD</option>
-							</select> </td></tr>
-							</table>
-							<body style="text-align:center">
-							<input type="hidden" name="currency_code" value="USD">
-							<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-							<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-							</form>
+						<h3 align="center"><strong> Subscriptions <strong></h3> <br>
 
-
+						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_xclick">
+						<input type="hidden" name="business" value="cjk4wr-facilitator@virginia.edu">
+						<input type="hidden" name="lc" value="US">
+						<input type="hidden" name="item_name" value="Subscriptions">
+						<input type="hidden" name="button_subtype" value="services">
+						<input type="hidden" name="no_note" value="0">
+						<input type="hidden" name="currency_code" value="USD">
+						<input type="hidden" name="tax_rate" value="10.000">
+						<input type="hidden" name="shipping" value="3.00">
+						<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+						<div class="container">
+						<table>
+						<tr><td><input type="hidden" name="on0" value="Subscriptions">Subscriptions</td></tr><tr><td><select name="os0">
+							<option value="Weekly Subscription">Weekly Subscription $9.99 USD</option>
+							<option value="Monthly Subscription">Monthly Subscription $29.99 USD</option>
+							<option value="Semester long Subscription">Semester long Subscription $99.99 USD</option>
+						</select> </td></tr>
+						</table>
+						<body style="text-align:center">
+						<input type="hidden" name="currency_code" value="USD">
+						<input type="hidden" name="option_select0" value="Weekly Subscription">
+						<input type="hidden" name="option_amount0" value="9.99">
+						<input type="hidden" name="option_select1" value="Monthly Subscription">
+						<input type="hidden" name="option_amount1" value="29.99">
+						<input type="hidden" name="option_select2" value="Semester long Subscription">
+						<input type="hidden" name="option_amount2" value="99.99">
+						<input type="hidden" name="option_index" value="0">
+						<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+						</form>
 							<?php } ?>
 							</div>
 				</div>
