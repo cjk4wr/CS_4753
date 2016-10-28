683154a0428741d292277b760860998dbf88e2e2
freshday957 <jgjung@Julies-MacBook-Pro-3.local>
2016-10-28 17:41:05 -0400

    remove feedback

--- transaction.php
+++ transaction.php
@@ -13,11 +13,11 @@ $cardissue = false;
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {	
 		if($_POST["date2"] == '16' && $_POST["date1"] < '12') {
-			$string = "There seems to be an issue with your card info. Please press the back button and confirm your card has not expired.";
+			$string = "There seems to be an issue with your card information. Please press the back button and confirm your card has not expired.";
 			$cardissue = true;
 		}
 		if($_POST["date2"] < '16') {
-			$string = "There seems to be an issue with your card info. Please press the back button and confirm your card has not expired.";
+			$string = "There seems to be an issue with your card information. Please press the back button and confirm your card has not expired.";
 			$cardissue = true;
 		}
 	}
@@ -196,65 +196,6 @@ $cardissue = false;
 							<?php } ?>
 							</div>
 				</div>
-
-			<!-- Footer -->
-				<div id="footer-wrapper">
-					<div id="footer" class="container">
-						<header class="major">
-							<h2>Give us feedback!</h2>
-							<p>Feel free to leave us a message here! We'll reply as fast as we can!</p>
-						</header>
-						<div class="row">
-							<section class="6u 12u(narrower)">
-								<form method="post" action="#">
-									<div class="row 50%">
-										<div class="6u 12u(mobile)">
-											<input name="name" placeholder="Name" type="text" />
-										</div>
-										<div class="6u 12u(mobile)">
-											<input name="email" placeholder="Email" type="text" />
-										</div>
-									</div>
-									<div class="row 50%">
-										<div class="12u">
-											<textarea name="message" placeholder="Message"></textarea>
-										</div>
-									</div>
-									<div class="row 50%">
-										<div class="12u">
-											<ul class="actions">
-												<li><input type="submit" value="Send Message" /></li>
-												<li><input type="reset" value="Clear form" /></li>
-											</ul>
-										</div>
-									</div>
-								</form>
-							</section>
-							<!-- Icons for social media;; commented out for now
-							<section class="6u 12u(narrower)">
-								<div class="row 0%">
-									<ul class="divided icons 6u 12u(mobile)">
-										<li class="icon fa-twitter"><a href="#"><span class="extra">twitter.com/</span>untitled</a></li>
-										<li class="icon fa-facebook"><a href="#"><span class="extra">facebook.com/</span>untitled</a></li>
-										<li class="icon fa-dribbble"><a href="#"><span class="extra">dribbble.com/</span>untitled</a></li>
-									</ul>
-									<ul class="divided icons 6u 12u(mobile)">
-										<li class="icon fa-instagram"><a href="#"><span class="extra">instagram.com/</span>untitled</a></li>
-										<li class="icon fa-youtube"><a href="#"><span class="extra">youtube.com/</span>untitled</a></li>
-										<li class="icon fa-pinterest"><a href="#"><span class="extra">pinterest.com/</span>untitled</a></li>
-									</ul>
-								</div>
-							</section>
-						-->
-						</div>
-					</div>
-					<div id="copyright" class="container">
-						<ul class="menu">
-							<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-						</ul>
-					</div>
-				</div>
-
 		</div>
 
 		<!-- Scripts -->
