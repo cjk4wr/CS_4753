32beed8ba99b06d846554d8a0e2872d246c4f767
freshday957 <jgjung@d-172-25-98-62.dhcp.virginia.edu>
2016-10-30 19:18:55 -0400

    email stuff

--- mailer.php
+++ mailer.php
@@ -6,13 +6,14 @@ include 'transaction.php'
 	$mail->isSMTP();                                   // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                            // Enable SMTP authentication
-	$mail->Username = 'Insert Email Address';          // SMTP username
-	$mail->Password = 'Insert Email Account Password'; // SMTP password
+	$mail->Username = 'CookEZalerts@gmail.com';          // SMTP username
+	$mail->Password = 'c00kezalerts'; // SMTP password
 	$mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                 // TCP port to connect to
 
-	$mail->setFrom('email@codexworld.com', 'CodexWorld');
-	$mail->addReplyTo($email, 'CodexWorld');
+	$mail->setFrom('CookEZalerts@gmail.com', 'CookEZ');
+	//$mail->addReplyTo($email, 'CodexWorld');
+	$mail->addReplyTo('CookEZalerts@gmail.com', 'CookEZ');
 	$mail->addAddress('john@gmail.com');   // Add a recipient
 	//$mail->addCC('cc@example.com');
 	//$mail->addBCC('bcc@example.com');
