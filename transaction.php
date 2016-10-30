7615996ea94206a8b22a59993c454dc8509a75a0
freshday957 <jgjung@d-172-25-98-62.dhcp.virginia.edu>
2016-10-30 19:37:37 -0400

    merge

--- transaction.php
+++ transaction.php
@@ -1,6 +1,12 @@
+<!--EComm Milestone
+
+- Make sure expiration date year is greater than 16
+- do we need to check for current date? (maybe a check on php: if(year == 16) check month or call error))
+- 
+-->
+
 <!-- Database input --> 
 <?php
-
 
 $string = "Congratulations! You are now a user! Feel free to browse our website!";
 $cardissue = false;
@@ -70,7 +76,7 @@ $cardissue = false;
 
 	$db->query("drop table cardinfo");
 
-	$result = $db->query("create table cardinfo (email varchar(255), cardnum varchar(16), expiration varchar(255), cvv varchar(4), FOREIGN KEY (email) REFERENCES userinfo(email) ON DELETE CASCADE)") or die ("Invalid: " . $db->error);
+	$result = $db->query("create table cardinfo (email varchar(255), cardnum varchar(16), expiration varchar(255), cvv varchar(3), FOREIGN KEY (email) REFERENCES userinfo(email) ON DELETE CASCADE)") or die ("Invalid: " . $db->error);
 
 	$email = $_POST["email"];
 			$card = $_POST["ccOne"] . $_POST["ccTwo"] . $_POST["ccThree"] . $_POST["ccFour"];
@@ -146,12 +152,15 @@ $cardissue = false;
 					<div class="container" id="main">
 
 						<!-- Content -->
+							<article id="content">
+								<header>
 									 <h3 align="center"><strong> <?php echo $string; ?> <strong></h3>
+								</header>
 								<!-- Originally 3 paragraphs using <p>; commented out b/c unnecessary for us as of now -->
+							</article>
 					</div>
 							<?php if($cardissue == false) {?>
 						<h3 align="center"><strong> Subscriptions <strong></h3> <br>
-
 						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 						<input type="hidden" name="cmd" value="_xclick">
 						<input type="hidden" name="business" value="cjk4wr-facilitator@virginia.edu">
