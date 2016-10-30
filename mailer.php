68302bccf2d574678014f4e3cde0dae9f87fb1f8
Cindy Park <csp9sm@virginia.edu>
2016-10-30 19:14:11 -0400

    from mailer.php take out paypal.php

--- mailer.php
+++ mailer.php
@@ -1,19 +1,6 @@
 <?php
 require 'PHPMailer/PHPMailerAutoload.php';
 include 'transaction.php'
-
-<?php require('PaypalIPN.php');
-use PaypalIPN;
-$ipn = new PayPalIPN();
-// Use the sandbox endpoint during testing.
-$ipn->useSandbox();
-$verified = $ipn->verifyIPN();
-if ($verified) {
-	/*
-     * Process IPN
-     * A list of variables is available here:
-     * https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNandPDTVariables/
-     */
 	$mail = new PHPMailer;
 
 	$mail->isSMTP();                                   // Set mailer to use SMTP
@@ -45,7 +32,4 @@ if ($verified) {
 	} else {
 	    echo 'Message has been sent';
 	}
-}
-// Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
-header("HTTP/1.1 200 OK");
 ?>
