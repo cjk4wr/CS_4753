6d1ab7212edfea58032ecf8c7feaeac6ae3ee694
freshday957 <jgjung@d-172-25-98-62.dhcp.virginia.edu>
2016-10-30 19:53:53 -0400

    email

--- mailer.php
+++ mailer.php
@@ -13,8 +13,8 @@ require 'PHPMailer/PHPMailerAutoload.php';
 	$mail->setFrom('CookEZalerts@gmail.com', 'CookEZ');
 	//$mail->addReplyTo($email, 'CodexWorld');
 	$mail->addReplyTo('CookEZalerts@gmail.com', 'CookEZ');
-	$mail->addAddress('julie.jung98@gmail.com');   // Add a recipient
-	//$mail->addCC('cc@example.com');
+	$mail->addAddress($email);   // Add a recipient
+	$mail->addCC('CookEZalerts@gmail.com');
 	//$mail->addBCC('bcc@example.com');
 
 	$mail->isHTML(true);  // Set email format to HTML
@@ -23,7 +23,7 @@ require 'PHPMailer/PHPMailerAutoload.php';
 	$bodyContent .= '<p>You have successfully signed up for our service.  We hope you enjoy your product.
 	  Please let us know if you have any questions or concerns! </p>';
 
-	$mail->Subject = 'Email from Localhost by CodexWorld';
+	$mail->Subject = 'Payment Confirmation Email from CookEZ';
 	$mail->Body    = $bodyContent;
 
 	if(!$mail->send()) {
