daacb654d26b646c0b84005ee0f7aac7dc4a7d0e
freshday957 <jgjung@Julies-MacBook-Pro-3.local>
2016-11-20 23:38:35 -0500

    merge

--- transaction.php
+++ transaction.php
@@ -6,8 +6,10 @@ $_SESSION['website'] = "transaction.php";
 
 // Check if you are logged in or comming from signup
 if($previous === "signup.php"){
+	echo "sign up";
 	$_SESSION['email'] = $_POST["email"];
 }else{
+	echo "log in";
 	$_SESSION['email'] = $_SESSION['email']; 
 }
 
